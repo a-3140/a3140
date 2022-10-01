a5166e107135d9fba9600f168e356e5e4e096c72
a-3140 <intpblank@gmail.com>
2022-10-01 18:41:25 +0800

    tech: Add unit testing ( jest ) to project
    Add sample unit test in a custom folder
    
    Add script for unit test on package.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,9 @@ const Home: NextPageWithLayout = () => {
     <div className="w-full min-h-screen flex justify-center items-center bg-neutral-900">
       <div className="max-w-5xl text-left">
         <div className="text-7xl font-semibold font-poppins text-stone-200 body-font tracking-wide hidden">
-          if you want something extra
+          This is the homepage
         </div>
-        <div className="text-center relative">
-          <p className="text-7xl text-[200px] font-semibold font-poppins body-font tracking-wide relative leading-none">
-            extra
-          </p>
-          <p className="absolute font-poppins font-extrabold -left-[200px] text-5xl top-20 text-stone-200 ">
-            This is
-          </p>
-        </div>
+        <div className="text-center relative" />
       </div>
     </div>
   );
