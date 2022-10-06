a08e05180ee2d00bed097b5bc6eae7c87cb1acac
a-3140 <intpblank@gmail.com>
2022-10-06 17:42:49 +0800

    Include www folder in content for tailwind
    Add nav and hero height on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./www/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    height: {
+      nav: "72px",
+      hero: "calc(100vh - 72px)",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
