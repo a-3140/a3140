950de55a76963775c5d54d18f195bbcd2961ecb1
a-3140 <intpblank@gmail.com>
2022-10-09 18:46:59 +0800

    Updated navbar with fluid animation on scroll and color hover on links

--- www/shared/layout/navbar/default-navbar.tsx
+++ www/shared/layout/navbar/default-navbar.tsx
@@ -19,9 +19,9 @@ export function Navbar() {
   return (
     <nav
       className={classNames(
-        scrolled && "bg-gray-200 py-4",
-        "transition-all duration-500",
-        "border-gray-200 px-2 sm:px-4 py-2.5 rounded h-nav fixed w-full z-50"
+        scrolled ? "bg-neutral-900 shadow-lg py-1 shadow-neutral-800" : "py-3",
+        "transition-all duration-700",
+        "border-gray-200 px-2 sm:px-4 rounded h-nav fixed w-full z-50"
       )}
     >
       <div className="container flex flex-wrap justify-between items-center mx-auto md:max-w-6xl">
@@ -30,8 +30,7 @@ export function Navbar() {
             <Image alt="a3140 logo" src="/logo.png" width={50} height={50} />
             <span
               className={classNames(
-                "transition-colors duration-500",
-                !scrolled || mobileOpen ? "text-gray-200" : "text-neutral-900",
+                "transition-colors delay-200 duration-700",
                 "self-center font-thin text-xl font-mono whitespace-nowrap text-gray-200 -ml-2"
               )}
             >
@@ -43,8 +42,7 @@ export function Navbar() {
           data-collapse-toggle="navbar-default"
           type="button"
           className={classNames(
-            "transition-colors duration-300",
-            !scrolled || mobileOpen ? "text-gray-200" : "text-neutral-900",
+            "transition-colors delay-200 duration-500 text-gray-200",
             "inline-flex items-center p-2 ml-3 text-sm rounded-lg transition-all ease-in-out z-50"
           )}
           aria-controls="navbar-default"
@@ -54,27 +52,30 @@ export function Navbar() {
           <span className="sr-only">Open main menu</span>
           <XMarkIcon
             className={classNames(
+              "hover:text-emerald-400",
               "h-6 w-6",
               "md:h-10 md:w-10",
-              "flex relative transition-all ease-in-out duration-300 rounded",
-              mobileOpen ? "opacity-100" : "opacity-0"
+              "flex relative transition-all ease-in-out duration-500 rounded",
+              mobileOpen ? "opacity-100 z-20" : "opacity-0 z-10"
             )}
           />
           <Bars3BottomRightIcon
             className={classNames(
+              "hover:text-emerald-400",
               "h-6 w-6",
               "md:h-10 md:w-10",
-              "absolute transition-all ease-in-out duration-300 rounded",
-              mobileOpen ? "opacity-0" : "opacity-100"
+              "absolute transition-all ease-in-out duration-500 rounded",
+              mobileOpen ? "opacity-0 z-10" : "opacity-100 z-20"
             )}
           />
         </button>
 
         <div className="w-full text-gray-200">
           <ul
+            onClick={() => setMobileOpen(!mobileOpen)}
             className={classNames(
               "md:text-4xl",
-              " transition-transform duration-300",
+              " transition-transform delay-200 duration-500",
               "fixed flex flex-col top-0 left-0",
               "font-mono font-bold text-2xl tracking-wide",
               mobileOpen ? "translate-x-0" : "translate-x-[100vw]",
@@ -84,7 +85,12 @@ export function Navbar() {
             {navItems.map((item) => (
               <li key={item.title}>
                 <Link href={item.link} passHref>
-                  <a href="passHref">{item.title}</a>
+                  <a
+                    href="passHref"
+                    className="transition-colors duration-300 ease-in-out hover:text-emerald-400"
+                  >
+                    {item.title}
+                  </a>
                 </Link>
               </li>
             ))}
